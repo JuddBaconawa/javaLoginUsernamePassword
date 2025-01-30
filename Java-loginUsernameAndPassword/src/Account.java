949986e1a63efd4405ce2967f70a90dad8f7a1b3
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-01-30 16:39:29 -0500

    deleted a class
    deleted char class

---
+++ Java-loginUsernameAndPassword/src/Account.java
@@ -0,0 +1,20 @@
+//imports
+
+//account class
+public class Account {
+
+    String username;
+    char[] password;
+
+
+    
+
+    public String getUsername() {
+        return username;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+    
+}
