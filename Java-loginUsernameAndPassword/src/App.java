6f88d474056f1b2face3b823c3bd8b39f97a8674
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-01-30 16:36:15 -0500

    added new class called Account and added some prompt for the scanner
    created prompt and added scanner + takes input via nextLine();

---
+++ Java-loginUsernameAndPassword/src/App.java
@@ -0,0 +1,29 @@
+//packages
+
+//imports
+import java.util.Scanner;
+
+//main class called App
+public class App {
+
+    //main method
+    public static void main(String[] args) throws Exception {
+        
+        //declare scanner for inputs
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter username: ");
+        String username = scanner.nextLine();
+
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+
+        //boolean logg
+
+
+
+        //scanner close
+        scanner.close();
+    }
+
+}
